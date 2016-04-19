5bf1468b57575c6909e5f5323259d33e08c1aec6
DavidBarron <david.barron91@gmail.com>
2016-04-19 17:59:53 -0500

    Cleaning up add buttons code

--- app/src/main/java/davidbar/foodwithfriends/ChooseFriendsActivity.java
+++ app/src/main/java/davidbar/foodwithfriends/ChooseFriendsActivity.java
@@ -15,6 +15,8 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
+import java.util.HashMap;
+
 public class ChooseFriendsActivity extends AppCompatActivity {
 
     private static final String TAG = "ChooseFriendsActivity";
@@ -22,6 +24,9 @@ public class ChooseFriendsActivity extends AppCompatActivity {
     static final int color_green = Color.parseColor("#32CD32");
     static final int color_grey = Color.parseColor("#a8a8a8");
 
+    private HashMap<String, String> mContacts = new HashMap<String, String>();
+
+    // Listener for buttons...
     private View.OnClickListener l = new View.OnClickListener(){
         public void onClick(View view){
 
@@ -43,47 +48,41 @@ public class ChooseFriendsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_friends);
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
-
-        //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        //fab.setOnClickListener(new View.OnClickListener() {
-        //    @Override
-        //    public void onClick(View view) {
-        //        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-        //                .setAction("Action", null).show();
-        //    }
-        //});
-        LinearLayout ll = (LinearLayout)findViewById(R.id.scroll_friends);
-        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-        lp.setMargins(10, 20, 30, 40);
 
         Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,null,null, null);
-        int idCount = 0;
+
         while (phones.moveToNext())
         {
             String name=phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
             String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
 
-            Button btn = new Button(this);
-            btn.setText(name);
-            btn.setId(idCount++);
-            btn.setBackgroundColor(color_grey);
-            btn.setOnClickListener(l);
-            //setMargins(btn,15,15,15,15);
-
-            ll.addView(btn,lp);
+            mContacts.put(name, phoneNumber);
 
             Log.d(TAG,name + " " + phoneNumber);
         }
         phones.close();
+
+        addFriendButtons(mContacts);
     }
 
-    private void setMargins (View view, int left, int top, int right, int bottom) {
-        if (view.getLayoutParams() instanceof ViewGroup.MarginLayoutParams) {
-            ViewGroup.MarginLayoutParams p = (ViewGroup.MarginLayoutParams) view.getLayoutParams();
-            p.setMargins(left, top, right, bottom);
-            view.requestLayout();
+    // Creates buttons in ScrollView from input HashMap
+    // Keys are contact names, Values are phone numbers
+    private void addFriendButtons(HashMap<String, String> map){
+
+        LinearLayout ll = (LinearLayout)findViewById(R.id.scroll_friends);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        lp.setMargins(15, 15, 15, 15);
+        int idCount = 0;
+
+        for (String key : map.keySet()){
+
+            Button button = new Button(this);
+            button.setText(key);
+            button.setId(idCount++);
+            button.setBackgroundColor(color_grey);
+            button.setOnClickListener(l);
+
+            ll.addView(button,lp);
         }
     }
 }
---
+++ app/src/main/java/davidbar/foodwithfriends/ContactsMagic.java
@@ -0,0 +1,20 @@
+package davidbar.foodwithfriends;
+
+import java.util.HashMap;
+
+/**
+ * Created by David on 4/19/2016.
+ */
+
+// Anything related to doing work with user contacts goes here.
+public class ContactsMagic {
+
+    //
+    public HashMap<String, String> getFriends(){
+        HashMap<String, String> retVal = new HashMap<String, String>();
+
+
+        return retVal;
+    }
+
+}
