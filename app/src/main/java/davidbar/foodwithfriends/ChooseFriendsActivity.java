d8e5f595fd56a8de99eb4f02ead55a0e37dbc156
DavidBarron <david.barron91@gmail.com>
2016-04-19 00:27:52 -0500

    Contacts buttons being dynamically generated.

--- app/src/main/java/davidbar/foodwithfriends/ChooseFriendsActivity.java
+++ app/src/main/java/davidbar/foodwithfriends/ChooseFriendsActivity.java
@@ -9,6 +9,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class ChooseFriendsActivity extends AppCompatActivity {
 
@@ -18,26 +20,42 @@ public class ChooseFriendsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_friends);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        //fab.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //    public void onClick(View view) {
+        //        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+        //                .setAction("Action", null).show();
+        //    }
+        //});
+        LinearLayout ll = (LinearLayout)findViewById(R.id.scroll_friends);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
         Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,null,null, null);
         while (phones.moveToNext())
         {
             String name=phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
             String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
 
+            Button btn = new Button(this);
+            btn.setText(name);
+
+            if(btn == null){
+                Log.d(TAG,"BUTTON NULL");
+            }
+            if(ll == null){
+                Log.d(TAG,"LAYOUT NULL");
+            }
+            if(lp == null){
+                Log.d(TAG,"LP NULL");
+            }
+
+            ll.addView(btn);
+
             Log.d(TAG,name + " " + phoneNumber);
         }
         phones.close();
     }
-
 }
