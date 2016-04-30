5e5f49c6c78d836c06e8b2c4ec988bcda433d244
DavidBarron <david.barron91@gmail.com>
2016-04-30 17:04:00 -0500

    Vibrator in send message button not working

--- app/src/main/java/davidbar/foodwithfriends/ResultActivity.java
+++ app/src/main/java/davidbar/foodwithfriends/ResultActivity.java
@@ -2,32 +2,32 @@ package davidbar.foodwithfriends;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.telephony.SmsManager;
+import android.view.HapticFeedbackConstants;
 import android.view.View;
 import android.widget.TextView;
 
+
 public class ResultActivity extends AppCompatActivity {
+
+    private boolean mMessageSent;
+
+    private String mName;
+    private String mAddress;
+    private String mPhone;
+
+    private Vibrator mVibrator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
 
-        //FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        //fab.setOnClickListener(new View.OnClickListener() {
-        //    @Override
-        //    public void onClick(View view) {
-        //        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-        //                .setAction("Action", null).show();
-        //    }
-        //});
-        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        mMessageSent = false;
+
+        mVibrator = (Vibrator) getApplicationContext().getSystemService(VIBRATOR_SERVICE);
 
         Intent thisIntent = getIntent();
 
@@ -35,13 +35,25 @@ public class ResultActivity extends AppCompatActivity {
         TextView addressText = (TextView) findViewById(R.id.result_address_blank);
         TextView phoneText = (TextView) findViewById(R.id.result_phone_blank);
 
-        nameText.setText((String)thisIntent.getSerializableExtra("name"));
-        addressText.setText((String)thisIntent.getSerializableExtra("address"));
-        phoneText.setText((String)thisIntent.getSerializableExtra("phone"));
+        mName = (String)thisIntent.getSerializableExtra("name");
+        mAddress = (String)thisIntent.getSerializableExtra("address");
+        mPhone = (String)thisIntent.getSerializableExtra("phone");
+
+        nameText.setText(mName);
+        addressText.setText(mAddress);
+        phoneText.setText(mPhone);
     }
 
     public void sendMessage(View view){
-        SmsManager smsManager = SmsManager.getDefault();
-        smsManager.sendTextMessage("817-269-5908", null, "Wooooooooo", null, null);
+
+        mVibrator.vibrate(HapticFeedbackConstants.VIRTUAL_KEY);
+
+        if (!mMessageSent){
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage("817-269-5908", null, "Wooooooooo", null, null);
+            mMessageSent = true;
+        }else{
+
+        }
     }
 }
