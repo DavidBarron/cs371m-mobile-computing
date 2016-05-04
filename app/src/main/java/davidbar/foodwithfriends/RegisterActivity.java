5813485c2bee0a9016a84fe22785f95f37c37616
DavidBarron <david.barron91@gmail.com>
2016-05-04 13:40:14 -0500

    User can manually register.

--- app/src/main/java/davidbar/foodwithfriends/RegisterActivity.java
+++ app/src/main/java/davidbar/foodwithfriends/RegisterActivity.java
@@ -1,5 +1,6 @@
 package davidbar.foodwithfriends;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,26 +8,52 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class RegisterActivity extends AppCompatActivity {
 
     private boolean phoneNumSet = false;
 
+    private EditText mEt;
+    private Toast mToast;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        String num = ContactsMagic.getMyPhoneNumber(this);
+        mEt = (EditText) findViewById(R.id.myPhoneNumberEditText);
 
-        if(!num.equals("PHONE_NUMBER_UNKNOWN")){
-            EditText et = (EditText) findViewById(R.id.myPhoneNumberEditText);
-            et.setText(ContactsMagic.formatPhoneNumber(num));
+        //String num = ContactsMagic.getMyPhoneNumber(this);
+
+        //if(!num.equals("PHONE_NUMBER_UNKNOWN")){
+        //    EditText et = (EditText) findViewById(R.id.myPhoneNumberEditText);
+        //    et.setText(ContactsMagic.formatPhoneNumber(num));
+        //}
+    }
+
+    public void clickOK(View view){
+        String num = ContactsMagic.formatPhoneNumber(mEt.getText().toString());
+
+        if (!num.equals("")){
+            KumulosWrapper.registerUser(this, num);
+            finish();
+        }else{
+            showToast("Invalid phone number.");
         }
     }
 
-    protected void registerUser(){
-        // TODO: something...
+    // Display toast with input text
+    private void showToast(String text){
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+
+        if(mToast != null){
+            mToast.cancel();
+        }
+
+        mToast = Toast.makeText(context, text, duration);
+        mToast.show();
     }
 }
